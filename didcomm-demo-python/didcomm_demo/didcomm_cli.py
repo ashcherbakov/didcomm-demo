0504b885c0c312050472d4d73fb881af7e7cd24f
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2021-10-01 12:25:10 +0300

    error handling in Python CLI

--- didcomm-demo-python/didcomm_demo/didcomm_cli.py
+++ didcomm-demo-python/didcomm_demo/didcomm_cli.py
@@ -24,14 +24,19 @@ def cli():
 @click.option('--service_endpoint', default=None, help='Optional service endpoint')
 @click.option('--service_routing_keys', default=[], help='Optional service routing keys')
 def create_peer_did(auth_keys_count, agreement_keys_count, service_endpoint, service_routing_keys):
+    click.echo()
     demo = DIDCommDemo(secrets_resolver)
-    did = demo.create_peer_did(
-        auth_keys_count=auth_keys_count,
-        agreement_keys_count=agreement_keys_count,
-        service_endpoint=service_endpoint,
-        service_routing_keys=service_routing_keys
-    )
-    click.echo(f"{did}")
+    try:
+        did = demo.create_peer_did(
+            auth_keys_count=auth_keys_count,
+            agreement_keys_count=agreement_keys_count,
+            service_endpoint=service_endpoint,
+            service_routing_keys=service_routing_keys
+        )
+        click.echo(f"{did}")
+    except Exception as e:
+        click.echo(f"{e}")
+    click.echo()
 
 
 @cli.command()
@@ -40,39 +45,56 @@ def create_peer_did(auth_keys_count, agreement_keys_count, service_endpoint, ser
               default="jwk",
               help='DID Doc format (JWK or Multibase)')
 def resolve_peer_did(did, format):
+    click.echo()
     format = DIDDocVerMaterialFormat.JWK if format == "jwk" else DIDDocVerMaterialFormat.MULTIBASE
-    did_doc_json = DIDCommDemo.resolve_peer_did(did, format)
-    click.echo(f"{did_doc_json}")
+    try:
+        did_doc_json = DIDCommDemo.resolve_peer_did(did, format)
+        click.echo(f"{did_doc_json}")
+    except Exception as e:
+        click.echo(f"{e}")
+    click.echo()
 
 
 @cli.command()
 @click.argument('msg')
 @click.option('--to', required=True, help="Receiver's DID")
-@click.option('--frm', default=None, help="Sender's DID. Anonymous encryption is used if not set.")
-@click.option('--sign-from', default=None, help="Sender's DID for optional signing. The message is not signed if not set.")
+@click.option('--from', 'frm', default=None, help="Sender's DID. Anonymous encryption is used if not set.")
+@click.option('--sign-from', default=None,
+              help="Sender's DID for optional signing. The message is not signed if not set.")
 @click.option('--protect-sender-id', default=True,
               help="Whether the sender's ID (DID) must be hidden. True by default.")
 def pack(msg, to, frm, sign_from, protect_sender_id):
+    click.echo()
     demo = DIDCommDemo(secrets_resolver)
-    res = demo.pack(
-        msg=msg,
-        to=to,
-        frm=frm,
-        sign_frm=sign_from,
-        config=PackEncryptedConfig(protect_sender_id=protect_sender_id)
-    )
-    click.echo(f"{res.packed_msg}")
+    try:
+        res = demo.pack(
+            msg=msg,
+            to=to,
+            frm=frm,
+            sign_frm=sign_from,
+            config=PackEncryptedConfig(protect_sender_id=protect_sender_id)
+        )
+        click.echo(f"{res.packed_msg}")
+    except Exception as e:
+        click.echo(f"{e}")
+    click.echo()
 
 
 @cli.command()
 @click.argument('msg')
 def unpack(msg):
-    demo = DIDCommDemo(secrets_resolver)
-    initial_msg, frm, to, _ = demo.unpack(msg)
-    if frm:
-        click.echo(f"authcrypted {initial_msg} from {frm} to {to}")
-    else:
-        click.echo(f"anoncrypted {initial_msg} to {to}")
+    click.echo()
+    try:
+        demo = DIDCommDemo(secrets_resolver)
+        initial_msg, frm, to, _ = demo.unpack(msg)
+        click.echo()
+        if frm:
+            click.echo(f"authcrypted {initial_msg} from {frm} to {to}")
+        else:
+            click.echo(f"anoncrypted {initial_msg} to {to}")
+    except Exception as e:
+        click.echo(f"{e}")
+    click.echo()
 
 
 if __name__ == '__main__':
